0b0aac776470aebcc706286838ec0cde4f96d9dc
Nolife <69518147+krkkrk11@users.noreply.github.com>
2023-06-27 22:06:47 +0700

    Update petsim.lua

--- petsim.lua
+++ petsim.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Rafacasari/roblox-scripts/main/psx.lua", true))()
 local Library = require(game:GetService("ReplicatedStorage").Library)
 local Orange = tonumber(game.Players.localPlayer.PlayerGui.Main.Boosts.Orange.TimeLeft.Text:split("x")[2])
-wait(30)
+wait(2)
 spawn(function()
     while wait(1) do
     	pcall(function()
@@ -12,7 +12,7 @@ spawn(function()
 				        if not Library.WorldCmds.Load("Diamond Mine") then return end
 				    end
 				    wait(2)
-				    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9020.01953125, 40, 2508.555908203125)
+				    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9020.01953125, 59, 2508.555908203125)
 				elseif Orange <= 100 and Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
 				    if Library.WorldCmds.Get() ~= "Pixel" then
 				        if not Library.WorldCmds.Load("Pixel") then return end
