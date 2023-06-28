563cdbd9c36fa742fe12a42e457074e83a944615
Nolife <69518147+krkkrk11@users.noreply.github.com>
2023-06-28 15:25:43 +0700

    make better petsim.lua

--- petsim.lua
+++ petsim.lua
@@ -4,24 +4,24 @@ wait(2)
 spawn(function()
     while wait(1) do
 	Orange = tonumber(game.Players.localPlayer.PlayerGui.Main.Boosts.Orange.TimeLeft.Text:split("x")[2])
-    	pcall(function()
-			if Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
-				if Orange >= 100 and Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
-				    if Library.WorldCmds.Get() ~= "Diamond Mine" then
-					wait(5)
-				        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9020.01953125, 59, 2508.555908203125)
-				    end
-				elseif Orange <= 100 and Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
-				    if Library.WorldCmds.Get() ~= "Pixel" then
-					wait(5)
-				        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3897, -16, 2440)
-				    end
-				end
-			--game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3100, 96, 137)
-			elseif Library.WorldCmds.Get() == "Fantasy" then
-		                wait(5)
-			        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3100, 96, 137)
-			end
-	end)
+        pcall(function()
+            if Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
+                if Orange >= 100 and Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
+                    if Library.WorldCmds.Get() ~= "Diamond Mine" then
+                    wait(5)
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9020.01953125, 59, 2508.555908203125)
+                    end
+                elseif Orange <= 100 and Library.WorldCmds.Get() ~= "Fantasy" and Library.WorldCmds.Get() ~= "Spawn" then
+                    if Library.WorldCmds.Get() ~= "Pixel" then
+                    wait(5)
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3897, -16, 2440)
+                    end
+                end
+            --game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3100, 96, 137)
+            elseif Library.WorldCmds.Get() == "Fantasy" then
+                wait(5)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3100, 96, 137)
+            end
+        end)
     end
 end)
